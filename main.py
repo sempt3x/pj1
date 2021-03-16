550867a7548bebc84fccec1f93200114d7f92387
sempt3x <32197873+sempt3x@users.noreply.github.com>
2021-03-16 12:58:17 +0100

    Update main.py
    Added better Format

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ ip_eingabe = ""
 port1 = -1
 port2 = 50
 start = 0
+correct_counter = 0
+fatal_counter = 0
 
 tn = telnetlib
 
@@ -17,13 +19,20 @@ port1 = int(port1)
 port2 = input("Enter the second port: ")
 port2 = int(port2)
 print("\n\n")
-
+print(Fore.WHITE+"[SCAN] \n")
 while port1 < port2:
     port1 += 1
     port_loop = format(port1)
     try:
         tn.Telnet(ip_eingabe, port_loop, 1)
         print(Fore.GREEN+"[TCP-Port: " + format(port_loop) + "] Port is open!")
+        correct_counter += 1
     except:
         print(Fore.RED+"[TCP-Port: " + format(port_loop) + "] Port is not open!")
+        fatal_counter += 1
+print("\n")
+print(Fore.WHITE+"[Status] \n")
+print("Used IP: "+ip_eingabe)
+print(Fore.GREEN+"Opened Ports: "+format(correct_counter))
+print(Fore.RED+"Closed Ports: "+format(fatal_counter))
 print(Fore.WHITE)
