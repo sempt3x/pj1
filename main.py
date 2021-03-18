e648baf4ca0bb4d8b9436dfad5c2d4f743830eab
sempt3x <32197873+sempt3x@users.noreply.github.com>
2021-03-18 07:54:07 +0100

    Replace of Chars
    Replaced all "" with ''

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import telnetlib
 from colorama import Fore
 
-ip_eingabe = ""
+ip_eingabe = ''
 port1 = 0
 port2 = 50
 start = 0
@@ -12,27 +12,27 @@ fatal_counter = 0
 tn = telnetlib
 
 ip_eingabe = input("Enter the IP address: ")
-os.system("ping " + ip_eingabe) == 1
-print("\b")
-port1 = input("Enter the first TCP port: ")
+os.system('ping ' + ip_eingabe) == 1
+print('\b')
+port1 = input('Enter the first TCP port: ')
 port1 = int(port1)
-port2 = input("Enter the second TCP port: ")
+port2 = input('Enter the second TCP port: ')
 port2 = int(port2)
 print("\n")
-print(Fore.WHITE+"[SCAN] \n")
+print(Fore.WHITE+'[SCAN] \n')
 while port1 < port2:
     port1 += 1
     port_loop = format(port1)
     try:
         tn.Telnet(ip_eingabe, port_loop, 1)
-        print(Fore.GREEN+"[TCP-Port: " + format(port_loop) + "] Port is open!")
+        print(Fore.GREEN+'[TCP-Port: ' + format(port_loop) + '] Port is open!')
         correct_counter += 1
     except:
-        print(Fore.RED+"[TCP-Port: " + format(port_loop) + "] Port is not open!")
+        print(Fore.RED+'[TCP-Port: ' + format(port_loop) + '] Port is not open!')
         fatal_counter += 1
 print("\n")
-print(Fore.WHITE+"[Status] \n")
-print("Used IP: "+ip_eingabe)
-print(Fore.GREEN+"Opened TCP Ports: "+format(correct_counter))
-print(Fore.RED+"Closed TCP Ports: "+format(fatal_counter))
+print(Fore.WHITE+'[Status] \n')
+print('Used IP: '+ip_eingabe)
+print(Fore.GREEN+'Opened TCP Ports: '+format(correct_counter))
+print(Fore.RED+'Closed TCP Ports: '+format(fatal_counter))
 print(Fore.WHITE)
